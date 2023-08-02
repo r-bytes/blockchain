5a58acc69f2d1bc5af547c6fdfe0c19175633b03
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-08-02 23:28:07 +0200

    feat: encrypt env file, updates

--- hardhat/ethers-simple-storage/deploy.ts
+++ hardhat/ethers-simple-storage/deploy.ts
@@ -1,47 +1,65 @@
 // prettier-ignore
 import { Contract, ContractFactory, ContractFunction, Wallet, ethers, } from "ethers";
-import { Block, BlockTag, Filter, FilterByBlockHash, Listener, Log, Provider, TransactionReceipt, TransactionRequest, TransactionResponse } from "@ethersproject/abstract-provider";
+import {
+  Block,
+  BlockTag,
+  Filter,
+  FilterByBlockHash,
+  Listener,
+  Log,
+  Provider,
+  TransactionReceipt,
+  TransactionRequest,
+  TransactionResponse,
+} from "@ethersproject/abstract-provider"
 
-import * as fs from "fs-extra";
-import "dotenv/config";
+import * as fs from "fs-extra"
+import "dotenv/config"
 
-async function main() {
+async function main(): Promise<void> {
   // url
-  const providerUrl: string = process.env.RPC_URL;
+  const providerUrl: string = process.env.RPC_URL
   // provider
   // prettier-ignore
   const provider: ethers.providers.JsonRpcProvider = new ethers.providers.JsonRpcProvider(providerUrl);
   // wallet
   const wallet: Wallet = new Wallet(process.env.PRIVATE_KEY!, provider);
+  // const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf-8")
+  // let wallet: Wallet = new Wallet.fromEncryptedJsonSync(
+  //   encryptedJson,
+  //   process.env.PRIVATE_KEY_PASSWORD
+  // );
   const abi: string = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.abi",
     "utf8"
-  );
+  )
   const binary: string = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
     "utf8"
-  );
+  )
 
-  const contractFactory = new ContractFactory(abi, binary, wallet);
-  console.log("====> Deploying, please wait...");
+  const contractFactory = new ContractFactory(abi, binary, wallet)
+  console.log("====> Deploying, please wait...")
 
   // /* deploy options */
   const deploymentOptions = {
     gasLimit: 2000000,
-  };
+  }
 
   /* deploy the actual contract */
-  const contract: Contract = await contractFactory.deploy(deploymentOptions);
-  console.log("=====> contract", contract);
+  const contract: Contract = await contractFactory.deploy(deploymentOptions)
+  console.log("=====> contract", contract)
 
   const transactionReceipt: ethers.providers.TransactionReceipt =
-    await contract.deployTransaction.wait(1);
+    await contract.deployTransaction.wait(1)
 
-  console.log("=====> This is the deployment transaction (transaction response): ");
-  console.log(contract.deployTransaction);
+  console.log(
+    "=====> This is the deployment transaction (transaction response): "
+  )
+  console.log(contract.deployTransaction)
 
-  console.log("=====> This is the transaction receipt (after first block): ");
-  console.log(transactionReceipt);
+  console.log("=====> This is the transaction receipt (after first block): ")
+  console.log(transactionReceipt)
 
   // /* DEPLOY WITH ONLY DATA */
   // console.log("=> deploy with only data (unlimited flexibility):")
@@ -66,21 +84,27 @@ async function main() {
 
   /* interact with contract functions */
   // retrieve initial favorite number
-  const currentFavoriteNumber: ContractFunction = await contract.retrieve();
-  console.log("=====> currentFavoriteNumber is: ", currentFavoriteNumber.toString());
-  
+  const currentFavoriteNumber: ContractFunction = await contract.retrieve()
+  console.log(
+    "=====> currentFavoriteNumber is: ",
+    currentFavoriteNumber.toString()
+  )
+
   // store a new favorite number
   const txResponse: TransactionResponse = await contract.store("7")
   const txReceipt: TransactionReceipt = await txResponse.wait(1)
 
   // retrieve the updated favorite number
-  const updatedFavoriteNumber: ContractFunction = await contract.retrieve();
-  console.log("=====> updatedFavoriteNumber is: ", updatedFavoriteNumber.toString());
+  const updatedFavoriteNumber: ContractFunction = await contract.retrieve()
+  console.log(
+    "=====> updatedFavoriteNumber is: ",
+    updatedFavoriteNumber.toString()
+  )
 }
 
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    console.error(error)
+    process.exit(1)
+  })
---
+++ hardhat/ethers-simple-storage/encryptKey.ts
@@ -0,0 +1,29 @@
+import { Wallet, ethers } from "ethers"
+import * as fs from "fs-extra"
+import "dotenv/config"
+
+async function main(): Promise<void> {
+    console.log(process.env.PRIVATE_KEY)
+    console.log(process.env.PRIVATE_KEY_PASSWORD)
+    
+    const wallet = new Wallet(process.env.PRIVATE_KEY!)
+
+    const encryptedJsonKey = await wallet.encrypt(
+        process.env.PRIVATE_KEY_PASSWORD!,
+        process.env.PRIVATE_KEY!
+    )
+    // In later version (^6.2.3 as of this commit) of etherjs, PRIVATE_KEY is inferred from wallet, so there is no need to 
+    // pass private key again. 
+    //     const encryptedJsonKey = await wallet.encrypt(
+    //         process.env.PRIVATE_KEY_PASSWORD!,
+    //  )
+    console.log(encryptedJsonKey)
+    fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
