77f8d6858912f4f770c1c470c8d23376c3f42172
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-08-06 18:42:01 +0200

    feat: deploy contract using hh deploy

--- hardhat-fund-me/deploy/deploy-fund-me.ts
+++ hardhat-fund-me/deploy/deploy-fund-me.ts
@@ -1,11 +1,17 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction } from "hardhat-deploy/types"
+import { deployments, network } from "hardhat"
 
-const deployFunc: DeployFunction = async function (
-  hre: HardhatRuntimeEnvironment,
-) {
-  // code here
-  console.log("=====> hi there")
+const deployFunc: DeployFunction = async function ({
+  getNamedAccounts,
+  deployments,
+}) {
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  const chainId = network.config.chainId
+
+  // what happens if we want to change chains
+  // when working with localhost or hh we want to use mocks
 }
 
 export default deployFunc
