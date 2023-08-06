10ef261d88fa56d433e537a0830eef6fe97719cb
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-08-06 23:07:20 +0200

    refactor: clean up

--- hardhat-fund-me/contracts/FundMe.sol
+++ hardhat-fund-me/contracts/FundMe.sol
@@ -5,75 +5,71 @@ import "./PriceConverter.sol";
 error NotOwner();
 
 contract FundMe {
-    // use PriceConverter as a library
-    using PriceConverter for uint256;
+  // use PriceConverter as a library
+  using PriceConverter for uint256;
 
-    uint256 public constant MINIMUN_USD = 50 * 1e18; // 1 * 10 ** 18
+  uint256 public constant MINIMUN_USD = 50 * 1e18; // 1 * 10 ** 18
 
-    address[] public funders;
-    address public immutable i_owner;
+  address[] public funders;
+  address public immutable i_owner;
 
-    mapping(address => uint256) public AddressToAmountFunded;
+  mapping(address => uint256) public AddressToAmountFunded;
 
-    AggregatorV3Interface public priceFeed;
+  AggregatorV3Interface public priceFeed;
 
-    constructor(address priceFeedAddress) {
-        i_owner = msg.sender;
-        priceFeed = AggregatorV3Interface(priceFeedAddress);
+  constructor(address priceFeedAddress) {
+    i_owner = msg.sender;
+    priceFeed = AggregatorV3Interface(priceFeedAddress);
+  }
+
+  // get funds from user
+  function fund() public payable {
+    // set a minimum amount of funding value in USD
+    require(
+      // this library automatically passes the ethAmount to msg.value (first param)
+      msg.value.getConversionRate(priceFeed) >= MINIMUN_USD,
+      "Didn't send enough"
+    );
+
+    // add funder to funders array and AddressToAmountFunded
+    funders.push(msg.sender);
+    AddressToAmountFunded[msg.sender] += msg.value;
+  }
+
+  // withdraw funds
+  function withdraw() public onlyOwner {
+    // loop over the funders array
+    for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+      // reset the ammount funded per index
+      address funder = funders[funderIndex];
+      AddressToAmountFunded[funder] = 0;
     }
+    // reset the actual funders array
+    funders = new address[](0);
 
-    // get funds from user
-    function fund() public payable {
-        // set a minimum amount of funding value in USD
-        require(
-            // this library automatically passes the ethAmount to msg.value (first param)
-            msg.value.getConversionRate(priceFeed) >= MINIMUN_USD,
-            "Didn't send enough"
-        );
+    // actually withdraw the funds using call
+    (bool callSuccess, ) = payable(msg.sender).call{
+      value: address(this).balance
+    }("");
+    require(callSuccess, "call failed");
+  }
 
-        // add funder to funders array and AddressToAmountFunded
-        funders.push(msg.sender);
-        AddressToAmountFunded[msg.sender] += msg.value;
+  // only owners can withdraw
+  modifier onlyOwner() {
+    // check this first
+    if (msg.sender == i_owner) {
+      revert NotOwner();
     }
+    // continue with the rest of the function
+    _;
+  }
 
-    // withdraw funds
-    function withdraw() public onlyOwner {
-        // loop over the funders array
-        for (
-            uint256 funderIndex = 0;
-            funderIndex < funders.length;
-            funderIndex++
-        ) {
-            // reset the ammount funded per index
-            address funder = funders[funderIndex];
-            AddressToAmountFunded[funder] = 0;
-        }
-        // reset the actual funders array
-        funders = new address[](0);
+  // senders should always use the fund function when sending eth
+  receive() external payable {
+    fund();
+  }
 
-        // actually withdraw the funds using call
-        (bool callSuccess, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
-        require(callSuccess, "call failed");
-    }
-
-    // only owners can withdraw
-    modifier onlyOwner() {
-        // check this first
-        if (msg.sender == i_owner) {
-            revert NotOwner();
-        }
-        // continue with the rest of the function
-        _;
-    }
-
-    // senders should always use the fund function when sending eth
-    receive() external payable {
-        fund();
-    }
-
-    fallback() external payable {
-        fund();
-    }
+  fallback() external payable {
+    fund();
+  }
 }
--- hardhat-fund-me/contracts/PriceConverter.sol
+++ hardhat-fund-me/contracts/PriceConverter.sol
@@ -4,19 +4,19 @@ pragma solidity 0.8.19;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-    // prettier-ignore
-    function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
-        (, int256 price, , , ) = priceFeed.latestRoundData();
-        // ETH in terms of USD
-        // 20000.00000000
-        return uint256(price * 1e10); // 1**10 == 10000000000
-    }
+  // prettier-ignore
+  function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
+    (, int256 price, , , ) = priceFeed.latestRoundData();
+    // ETH in terms of USD
+    // 20000.00000000
+    return uint256(price * 1e10); // 1**10 == 10000000000
+  }
 
-    // prettier-ignore
-    function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed) internal view returns (uint256) {
-      uint256 ethPrice = getPrice(priceFeed);
+  // prettier-ignore
+  function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed) internal view returns (uint256) {
+    uint256 ethPrice = getPrice(priceFeed);
 
-      uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
-      return ethAmountInUSD;
-    }
+    uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
+    return ethAmountInUSD;
+  }
 }
