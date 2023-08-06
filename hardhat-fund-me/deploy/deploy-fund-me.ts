1867bd5f1a94393c23f54f2f29c3d89960700189
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-08-06 23:07:35 +0200

    feat: deploy func

--- hardhat-fund-me/deploy/deploy-fund-me.ts
+++ hardhat-fund-me/deploy/deploy-fund-me.ts
@@ -1,6 +1,7 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
-import { DeployFunction } from "hardhat-deploy/types"
+import { DeployFunction, DeployResult } from "hardhat-deploy/types"
 import { deployments, network } from "hardhat"
+import networkConfig from "../helper-hardhat-config"
 
 const deployFunc: DeployFunction = async function ({
   getNamedAccounts,
@@ -8,10 +9,26 @@ const deployFunc: DeployFunction = async function ({
 }) {
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
-  const chainId = network.config.chainId
+  const chainId: number = network.config.chainId!
+
+  console.log("=====>", chainId, typeof(chainId))
+  const ethUsdPriceFeed = networkConfig[chainId]["ethUsdPriceFeed"]
+  console.log("=====>", networkConfig[chainId]["ethUsdPriceFeed"])
+  console.log("=====> ethUsdPriceFeed", ethUsdPriceFeed)
+  // if (chainId == 31337) {
+  //   const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+  //   ethUsdPriceFeedAddress = ethUsdAggregator.address
+  // } else {
+    // ethUsdPriceFeedAddress = networkConfig[network.name].ethUsdPriceFeed!
+  // }
 
   // what happens if we want to change chains
   // when working with localhost or hh we want to use mocks
+  const fundMe: DeployResult = await deploy("FundMe", {
+    from: deployer,
+    args: [], // priceFeedAddress
+    log: true,
+  })
 }
 
 export default deployFunc
