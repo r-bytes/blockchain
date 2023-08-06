d66e692017dd75a47ee4a353665c3bc40a30e871
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-08-06 12:18:53 +0200

    refactor: dynamic pricefeed

--- hardhat-fund-me/contracts/FundMe.sol
+++ hardhat-fund-me/contracts/FundMe.sol
@@ -1,73 +1,79 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
-
 import "./PriceConverter.sol";
 
 error NotOwner();
 
 contract FundMe {
+    // use PriceConverter as a library
     using PriceConverter for uint256;
 
     uint256 public constant MINIMUN_USD = 50 * 1e18; // 1 * 10 ** 18
 
     address[] public funders;
-    mapping(address => uint256) public AddressToAmountFunded;
-
     address public immutable i_owner;
 
-    constructor() {
+    mapping(address => uint256) public AddressToAmountFunded;
+
+    AggregatorV3Interface public priceFeed;
+
+    constructor(address priceFeedAddress) {
         i_owner = msg.sender;
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
 
     // get funds from user
-    // withdraw funds
-
     function fund() public payable {
         // set a minimum amount of funding value in USD
-        // 1. how do we send WTH to this contract
-        require(msg.value.getConversionRate() >= MINIMUN_USD, "Didn't send enough"); // 1e18 == 1 * 10 ** 18 == 1000000000000000000 wei = 1 ether
-        // what is reverting? => undo any action before, and send remaining gas back
-        // 18 decimals
+        require(
+            // this library automatically passes the ethAmount to msg.value (first param)
+            msg.value.getConversionRate(priceFeed) >= MINIMUN_USD,
+            "Didn't send enough"
+        );
+
+        // add funder to funders array and AddressToAmountFunded
         funders.push(msg.sender);
         AddressToAmountFunded[msg.sender] += msg.value;
     }
 
+    // withdraw funds
     function withdraw() public onlyOwner {
-        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+        // loop over the funders array
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            // reset the ammount funded per index
             address funder = funders[funderIndex];
             AddressToAmountFunded[funder] = 0;
         }
-        // reset the array
+        // reset the actual funders array
         funders = new address[](0);
 
-        // actually withdraw the funds (there are three options)
-
-        // option 1 = transfer 
-        // gas limit is capped to 2300, throws an error
-        // payable(msg.sender).transfer(address(this).balance);
-
-        // option 2. send
-        // gas limit is capped to 2300, return an bool
-        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
-        // require(sendSuccess, "send failed");
-        
-        // option 3 - call (recommended way)
-        // forward all gas or set gas, returns bool
-        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        // actually withdraw the funds using call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
         require(callSuccess, "call failed");
     }
 
-    modifier onlyOwner {
-        // require(msg.sender == i_owner, "Sender is not owner");
-        if (msg.sender == i_owner) { revert NotOwner(); }
+    // only owners can withdraw
+    modifier onlyOwner() {
+        // check this first
+        if (msg.sender == i_owner) {
+            revert NotOwner();
+        }
+        // continue with the rest of the function
         _;
     }
 
-    receive() external payable{
-       fund();
+    // senders should always use the fund function when sending eth
+    receive() external payable {
+        fund();
     }
 
-    fallback() external payable{
+    fallback() external payable {
         fund();
     }
 }
--- hardhat-fund-me/contracts/PriceConverter.sol
+++ hardhat-fund-me/contracts/PriceConverter.sol
@@ -4,33 +4,26 @@ pragma solidity 0.8.19;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
-  function getPrice() internal view returns (uint256) {
-    // ABI
-    // Adddress
-    // 0x694AA1769357215DE4FAC081bf1f309aDC325306
-    AggregatorV3Interface priceFeed = AggregatorV3Interface(
-      0x694AA1769357215DE4FAC081bf1f309aDC325306
-    );
-    (, int256 price, , , ) = priceFeed.latestRoundData();
-    // ETH in terms of USD
-    // 20000.00000000
-    return uint256(price * 1e10); // 1**10 == 10000000000
-  }
+    // prettier-ignore
+    function getPrice(AggregatorV3Interface priceFeed) internal view returns (uint256) {
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        // 20000.00000000
+        return uint256(price * 1e10); // 1**10 == 10000000000
+    }
 
-  function getVersion() internal view returns (uint256) {
-    AggregatorV3Interface priceFeed = AggregatorV3Interface(
-      0x694AA1769357215DE4FAC081bf1f309aDC325306
-    );
-    return priceFeed.version();
-  }
+    function getVersion() internal view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        return priceFeed.version();
+    }
 
-  function getConversionRate(
-    uint256 ethAmount
-  ) internal view returns (uint256) {
-    uint256 ethPrice = getPrice();
-    // 2000_000000000000000000 1e18 = ETH / USD price
-    // 1_000000000000000000 ETH
-    uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
-    return ethAmountInUSD;
-  }
+    // prettier-ignore
+    function getConversionRate(uint256 ethAmount, AggregatorV3Interface priceFeed) internal view returns (uint256) {
+      uint256 ethPrice = getPrice(priceFeed);
+
+      uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
+      return ethAmountInUSD;
+    }
 }
