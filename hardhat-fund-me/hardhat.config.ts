0051b46242240123b067d811c08a9017adf1836b
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-08-07 10:06:12 +0200

    config: add solidity compilers

--- hardhat-fund-me/hardhat.config.ts
+++ hardhat-fund-me/hardhat.config.ts
@@ -12,45 +12,48 @@ const PRIVATE_KEY_GANACHE: string = process.env.PRIVATE_KEY_GANACHE!
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY!
 
 const config: HardhatUserConfig = {
-    solidity: "0.8.19",
-    defaultNetwork: "hardhat",
-    networks: {
-        hardhat: {},
-        sepolia: {
-            url: RPC_URL_SEPOLIA,
-            accounts: [PRIVATE_KEY],
-            chainId: 11155111,
-        },
-        ganache: {
-            url: RPC_URL_GANACHE,
-            accounts: [PRIVATE_KEY_GANACHE],
-            chainId: 1337,
-        },
-        localhost: {
-            url: "http://127.0.0.1:8545/",
-            chainId: 31337,
-        },
+  // solidity: "0.8.19",
+  solidity: {
+    compilers: [{ version: "0.8.19" }, { version: "0.6.6" }],
+  },
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: RPC_URL_SEPOLIA,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
     },
-    etherscan: {
-        apiKey: ETHERSCAN_API_KEY,
+    ganache: {
+      url: RPC_URL_GANACHE,
+      accounts: [PRIVATE_KEY_GANACHE],
+      chainId: 1337,
     },
-    gasReporter: {
-        enabled: true,
-        outputFile: "report-gas.txt",
-        noColors: true,
-        // currency: "USD",
-        // coinmarketcap: COINMARKETCAP_API_KEY,
-        token: "MATIC",
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
     },
-    namedAccounts: {
-        deployer: {
-            default: 0,
-            11155111: 1
-        },
-        user: {
-            default: 1,
-        }
-    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    outputFile: "report-gas.txt",
+    noColors: true,
+    // currency: "USD",
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "MATIC",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+      11155111: 1,
+    },
+    user: {
+      default: 1,
+    },
+  },
 }
 
 export default config
