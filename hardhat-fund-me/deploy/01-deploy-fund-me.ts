e498f78ee41617bfac94ac17626a8559a0a1a2fb
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-08-08 20:52:21 +0200

    feast: verify when on main or test networks

--- hardhat-fund-me/deploy/01-deploy-fund-me.ts
+++ hardhat-fund-me/deploy/01-deploy-fund-me.ts
@@ -1,18 +1,17 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { DeployFunction, DeployResult } from "hardhat-deploy/types"
-import { deployments, network } from "hardhat"
 import { developmentChains, networkConfig } from "../helper-hardhat-config"
+import { verify } from "../utils/verify"
 
 // prettier-ignore
-const deployFunc: DeployFunction = async ({ getNamedAccounts, deployments }) => {
+const deployFunc: DeployFunction = async ({ getNamedAccounts, deployments, network }) => {
   const { deploy, log, get } = deployments
   const { deployer } = await getNamedAccounts()
-  
-  let ethUsdPriceFeed: string = "";
   const chainId: number = network.config.chainId!
+
+  let ethUsdPriceFeed: string = ""
   log("=====> network is: ", network.name)
   log("=====> chainId is: ", chainId)
-
 
   // check if we are on a development chain
   // if so, we should use mocks
@@ -24,20 +23,27 @@ const deployFunc: DeployFunction = async ({ getNamedAccounts, deployments }) => 
     ethUsdPriceFeed = networkConfig[chainId]["ethUsdPriceFeed"]
   }
   log("")
-  log("=====> ----------------------------------------------------------------")
+  log("----------------------------------------------------------------")
 
   log("")
   log("=====> deploying FundMe and waiting for confirmations...")
+
+  log("=====> debug: ", deployer, ethUsdPriceFeed)
   const fundMe: DeployResult = await deploy("FundMe", {
     from: deployer,
-    args: [ethUsdPriceFeed], // priceFeedAddress
+    args: [ethUsdPriceFeed], // priceFeedAddress goes here
     log: true,
+    waitConfirmations: 6
   })
+
+  if (!developmentChains.includes("network.name") && process.env.ETHERSCAN_API_KEY) {
+    // verify the contract
+    await verify(fundMe.address, [ethUsdPriceFeed])
+  }
 
   // log("=====> receipt: ", fundMe.receipt)
   log("")
-  log("=====> ----------------------------------------------------------------")
-
+  log("----------------------------------------------------------------")
 }
 
 export default deployFunc
