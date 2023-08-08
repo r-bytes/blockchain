317250166b2ab92822e03bca52dc7f9b3e899eee
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-08-08 20:51:34 +0200

    feat: verify util file

---
+++ hardhat-fund-me/utils/verify.ts
@@ -0,0 +1,18 @@
+import { run } from "hardhat"
+
+export const verify = async (contractAddress: string, args: string[]): Promise<void> => {
+  console.log("=====> verifying contract...")
+
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    })
+  } catch (error: any) {
+    if (error.message.toLowerCase().includes("=====> already verified")) {
+      console.log("=====> verification failed")
+    } else {
+      console.log(error)
+    }
+  }
+}
