88cd7a78d380dd89b7250e8fbb878d6766d4d7de
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-08-09 23:09:29 +0200

    test: unit testing

--- hardhat-fund-me/contracts/FundMe.sol
+++ hardhat-fund-me/contracts/FundMe.sol
@@ -12,72 +12,76 @@ error FundMe__NotOwner();
  *  @dev functions order: constructor, receive, fallback, external, public, internal, private, view, pure
  */
 contract FundMe {
-  // libraries
-  using PriceConverter for uint256;
+    // libraries
+    using PriceConverter for uint256;
 
-  // state variables
-  mapping(address => uint256) public AddressToAmountFunded;
-  address[] public funders;
-  address public immutable i_owner;
-  uint256 public constant MINIMUN_USD = 50 * 1e18; // 1 * 10 ** 18
-  AggregatorV3Interface public priceFeed;
+    // state variables
+    mapping(address => uint256) public AddressToAmountFunded;
+    address[] public funders;
+    address public immutable i_owner;
+    uint256 public constant MINIMUN_USD = 50 * 1e18; // 1 * 10 ** 18
+    AggregatorV3Interface public priceFeed;
 
-  // modifiers
-  modifier onlyOwner() {
-    if (msg.sender == i_owner) {
-      revert FundMe__NotOwner();
+    // modifiers
+    modifier onlyOwner() {
+        if (msg.sender == i_owner) {
+            revert FundMe__NotOwner();
+        }
+        _;
     }
-    _;
-  }
 
-  constructor(address priceFeedAddress) {
-    i_owner = msg.sender;
-    priceFeed = AggregatorV3Interface(priceFeedAddress);
-  }
-
-  // senders should always use the fund function when sending eth
-  receive() external payable {
-    fund();
-  }
-
-  fallback() external payable {
-    fund();
-  }
-
-  /**
-   *  @notice This function funds this contract
-   *  @dev This implements price feeds as a library
-   */
-  
-  // get funds from user
-  function fund() public payable {
-    require(
-      // set a minimum amount of funding value in USD
-      // this library automatically passes the ethAmount to msg.value (first param)
-      msg.value.getConversionRate(priceFeed) >= MINIMUN_USD,
-      "Didn't send enough"
-    );
-
-    // add funder to funders array and AddressToAmountFunded
-    funders.push(msg.sender);
-    AddressToAmountFunded[msg.sender] += msg.value;
-  }
-
-  // withdraw funds
-  function withdraw() public onlyOwner {
-    // loop over the funders array
-    for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
-      // reset the ammount funded per index
-      address funder = funders[funderIndex];
-      AddressToAmountFunded[funder] = 0;
+    constructor(address priceFeedAddress) {
+        i_owner = msg.sender;
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
     }
-    // reset the actual funders array
-    funders = new address[](0);
 
-    // actually withdraw the funds using call
-    (bool callSuccess, ) = payable(msg.sender).call{
-      value: address(this).balance
-    }("");
-    require(callSuccess, "call failed");
-  }
+    // senders should always use the fund function when sending eth
+    receive() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    /**
+     *  @notice This function funds this contract
+     *  @dev This implements price feeds as a library
+     */
+
+    // get funds from user
+    function fund() public payable {
+        require(
+            // set a minimum amount of funding value in USD
+            // this library automatically passes the ethAmount to msg.value (first param)
+            msg.value.getConversionRate(priceFeed) >= MINIMUN_USD,
+            "You need to spend more ETH!"
+        );
+
+        // add funder to funders array and AddressToAmountFunded
+        funders.push(msg.sender);
+        AddressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    // withdraw funds
+    function withdraw() public onlyOwner {
+        // loop over the funders array
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            // reset the ammount funded per index
+            address funder = funders[funderIndex];
+            AddressToAmountFunded[funder] = 0;
+        }
+        // reset the actual funders array
+        funders = new address[](0);
+
+        // actually withdraw the funds using call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "call failed");
+    }
 }
--- hardhat-fund-me/test/unit/FundMe.test.ts
+++ hardhat-fund-me/test/unit/FundMe.test.ts
@@ -4,57 +4,90 @@ import { assert, expect } from "chai";
 import { developmentChains } from "../../helper-hardhat-config";
 import { log } from "console";
 import { Address, DeployFunction } from "hardhat-deploy/types";
+import { ContractTransactionResponse } from "ethers";
 
 describe("FundMe", async () => {
-  let fundMe: FundMe;
-  let deployer: Address;
-  let mockV3Aggregator: MockV3Aggregator;
+    let fundMe: FundMe;
+    let deployer: Address;
+    let mockV3Aggregator: MockV3Aggregator;
 
-  // first let's deploy our contract
-  beforeEach(async () => {
-    if (!developmentChains.includes(network.name)) {
-      throw "=====> you need to be on a development chain to run tests";
-    }
-    // get deployer
-    deployer = (await getNamedAccounts()).deployer;
-    // deploy all contracts
-    const deploymentResults = await deployments.fixture(["all"]);
-    /* NOT WORKING
-      // get contract by name
-      // fundMe = await ethers.getContractAt("FundMe", deployer); 
-    */
+    const sendValue = ethers.parseEther("1"); // 1e18
 
-    // get deployer address from deployment results
-    // log("=====> deploymentResults 1: ", deploymentResults["FundMe"])
-    // log("=====> deploymentResults 2: ", deploymentResults["FundMe"]["receipt"]?.from) // 66
-    // log("=====> deploymentResults 3: ", deploymentResults["FundMe"]?.address) // 12
-    // log("=====> deploymentResults 4: ", deployer) // 66
-    const fundMeAddress: Address = deploymentResults["FundMe"]?.address; // 12
-    const mockV3AggregatorAddress: Address =
-      deploymentResults["MockV3Aggregator"]?.address;
+    // first let's deploy our contract
+    beforeEach(async () => {
+        if (!developmentChains.includes(network.name)) {
+            throw "=====> you need to be on a development chain to run tests";
+        }
+        // get deployer
+        deployer = (await getNamedAccounts()).deployer;
+        // deploy all contracts
+        const deploymentResults = await deployments.fixture(["all"]);
+        /* NOT WORKING
+            // get contract by name
+            // fundMe = await ethers.getContractAt("FundMe", deployer); 
+        */
 
-    fundMe = await ethers.getContractAt("FundMe", fundMeAddress);
-    mockV3Aggregator = await ethers.getContractAt(
-      "MockV3Aggregator",
-      mockV3AggregatorAddress,
-    );
-  });
+        // get deployer address from deployment results
+        // log("=====> deploymentResults 1: ", deploymentResults["FundMe"])
+        // log("=====> deploymentResults 2: ", deploymentResults["FundMe"]["receipt"]?.from) // 66
+        // log("=====> deploymentResults 3: ", deploymentResults["FundMe"]?.address) // 12
+        // log("=====> deploymentResults 4: ", deployer) // 66
+        const fundMeAddress: Address = deploymentResults["FundMe"]?.address; // 12
+        const mockV3AggregatorAddress: Address =
+            deploymentResults["MockV3Aggregator"]?.address;
 
-  describe("constructor", async () => {
-    it("sets the aggregator addresses correctly", async () => {
-      const priceFeedAddress: Address = await fundMe.priceFeed();
-      const mockV3AggregatorAddress: string =
-        await mockV3Aggregator.getAddress();
-
-      assert.equal(priceFeedAddress, mockV3AggregatorAddress);
+        fundMe = await ethers.getContractAt("FundMe", fundMeAddress);
+        mockV3Aggregator = await ethers.getContractAt(
+            "MockV3Aggregator",
+            mockV3AggregatorAddress,
+        );
     });
-  });
 
-  describe("fund", async () => {
-    it("fails if you don't send enough eth", async () => {
-      await expect(fundMe.fund()).to.be.revertedWith(
-        "=====> you need to send more eth",
-      );
+    describe("constructor", async () => {
+        it("sets the aggregator addresses correctly", async () => {
+            const priceFeedAddress: Address = await fundMe.priceFeed();
+            const mockV3AggregatorAddress: Address =
+                await mockV3Aggregator.getAddress();
+
+            assert.equal(priceFeedAddress, mockV3AggregatorAddress);
+        });
     });
-  });
+
+    describe("fund", async () => {
+        it("fails if you don't send enough eth", async () => {
+            await expect(fundMe.fund()).to.be.revertedWith(
+                "You need to spend more ETH!",
+            );
+        });
+        it("updated the amountFunded data structure", async () => {
+            const txResponse: ContractTransactionResponse = await fundMe.fund({
+                value: sendValue,
+            });
+            const response: bigint = await fundMe.AddressToAmountFunded(
+                deployer,
+            );
+
+            // assert.equal(txResponse.from, deployer)
+            assert.equal(response, sendValue);
+        });
+        it("should add funder to array of funders", async () => {
+            const txResponse: ContractTransactionResponse = await fundMe.fund({
+                value: sendValue,
+            });
+            const funder: Address = await fundMe.funders(0);
+
+            assert(funder, deployer);
+        });
+    });
+    describe("should withraw the funds from the contract", async () => {
+        // fund the contract before we test 'it
+        beforeEach(async () => {
+            const txResponse: ContractTransactionResponse = await fundMe.fund({
+                value: sendValue,
+            });
+        });
+        it("should add", async () => {
+            ("");
+        });
+    });
 });
