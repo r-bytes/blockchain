7ae4bf72ea2a7c33f57c15923f7360728661d0ac
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-08-09 22:14:09 +0200

    test: unit testing

--- hardhat-fund-me/test/unit/FundMe.test.ts
+++ hardhat-fund-me/test/unit/FundMe.test.ts
@@ -1,6 +1,6 @@
-import { deployments, ethers, getNamedAccounts, network } from "hardhat"
-import { FundMe, MockV3Aggregator } from "../../typechain-types"
-import { assert } from "chai";
+import { deployments, ethers, getNamedAccounts, network } from "hardhat";
+import { FundMe, MockV3Aggregator } from "../../typechain-types";
+import { assert, expect } from "chai";
 import { developmentChains } from "../../helper-hardhat-config";
 import { log } from "console";
 import { Address, DeployFunction } from "hardhat-deploy/types";
@@ -13,12 +13,12 @@ describe("FundMe", async () => {
   // first let's deploy our contract
   beforeEach(async () => {
     if (!developmentChains.includes(network.name)) {
-      throw "=====> you need to be on a development chain to run tests"
+      throw "=====> you need to be on a development chain to run tests";
     }
     // get deployer
-    deployer = (await getNamedAccounts()).deployer
+    deployer = (await getNamedAccounts()).deployer;
     // deploy all contracts
-    const deploymentResults = await deployments.fixture(["all"])
+    const deploymentResults = await deployments.fixture(["all"]);
     /* NOT WORKING
       // get contract by name
       // fundMe = await ethers.getContractAt("FundMe", deployer); 
@@ -30,18 +30,31 @@ describe("FundMe", async () => {
     // log("=====> deploymentResults 3: ", deploymentResults["FundMe"]?.address) // 12
     // log("=====> deploymentResults 4: ", deployer) // 66
     const fundMeAddress: Address = deploymentResults["FundMe"]?.address; // 12
-    const mockV3AggregatorAddress: Address = deploymentResults["MockV3Aggregator"]?.address;
-    
+    const mockV3AggregatorAddress: Address =
+      deploymentResults["MockV3Aggregator"]?.address;
+
     fundMe = await ethers.getContractAt("FundMe", fundMeAddress);
-    mockV3Aggregator = await ethers.getContractAt("MockV3Aggregator", mockV3AggregatorAddress)
-  })
+    mockV3Aggregator = await ethers.getContractAt(
+      "MockV3Aggregator",
+      mockV3AggregatorAddress,
+    );
+  });
 
   describe("constructor", async () => {
     it("sets the aggregator addresses correctly", async () => {
       const priceFeedAddress: Address = await fundMe.priceFeed();
-      const mockV3AggregatorAddress: string = await mockV3Aggregator.getAddress();
+      const mockV3AggregatorAddress: string =
+        await mockV3Aggregator.getAddress();
 
       assert.equal(priceFeedAddress, mockV3AggregatorAddress);
-    })
-  })
-})
+    });
+  });
+
+  describe("fund", async () => {
+    it("fails if you don't send enough eth", async () => {
+      await expect(fundMe.fund()).to.be.revertedWith(
+        "=====> you need to send more eth",
+      );
+    });
+  });
+});
