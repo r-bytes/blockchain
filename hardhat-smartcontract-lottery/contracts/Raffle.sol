389762cfe0727bba2059c2b3e820d8fc9562de0f
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-08-21 07:51:21 +0200

    feat: request random winner function

--- hardhat-smartcontract-lottery/contracts/Raffle.sol
+++ hardhat-smartcontract-lottery/contracts/Raffle.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-// import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 // import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
@@ -22,13 +22,30 @@ contract Raffle is VRFConsumerBaseV2 {
     // state variables
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private immutable i_callbackGasLimit;
+    uint32 private constant NUMWORDS = 1;
 
     // events
     // naming convention function name reversed
     event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
 
-    constructor(address vrfCoordinatorV2, uint256 entranceFee) VRFConsumerBaseV2(vrfCoordinatorV2) {
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() public payable {
@@ -45,6 +62,17 @@ contract Raffle is VRFConsumerBaseV2 {
     function requestRandomWinner() external {
         // request random number
         // once we get it, do something with it
+
+        // Will revert if subscription is not set and funded.
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, // gasLane
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUMWORDS
+        );
+
+        emit RequestedRaffleWinner(requestId);
     }
 
     function fulfillRandomWords(
