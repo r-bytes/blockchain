4345a26de0ed061ee2f36b89784d47cd2c5dcd9b
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-09-06 22:12:31 +0200

    feat: added moralis provider

--- nextjs-smartcontract-lottery/src/app/page.tsx
+++ nextjs-smartcontract-lottery/src/app/page.tsx
@@ -1,9 +1,20 @@
+"use client"
+import { MoralisProvider } from "react-moralis";
+import { Header, ManualHeader } from "./components";
+
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                fff
-            </div>
-        </main>
+        <>
+        <MoralisProvider initializeOnMount={false}>
+
+            {/* <ManualHeader /> */}
+            <Header />
+            <main className="flex min-h-screen flex-col items-center justify-between p-24">
+                <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+                    This home
+                </div>
+            </main>
+        </MoralisProvider>
+        </>
     );
 }
