5320278bf0888137b804fb5e1dc25d116a9ecb9a
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-09-06 22:12:10 +0200

    feat: add head with w3uikit

---
+++ nextjs-smartcontract-lottery/src/app/components/Header.tsx
@@ -0,0 +1,10 @@
+import { ConnectButton } from "@web3uikit/web3";
+
+const Header = () => {
+    return (
+        <div>
+            <ConnectButton moralisAuth={false} />
+        </div>
+    );
+};
+export default Header;
