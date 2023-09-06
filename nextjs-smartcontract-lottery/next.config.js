633d4965109f4df843c9975c8e9223491a433f0c
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-09-06 22:11:26 +0200

    build: initial project setup

--- nextjs-smartcontract-lottery/next.config.js
+++ nextjs-smartcontract-lottery/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    presets: ["next/babel"],
+    plugins: [["styled-components", { ssr: true }]],
+    pageExtensions: ["ts", "tsx", "mdx"],
+    experimental: {
+        mdxRs: true,
+    },
+};
 
 module.exports = nextConfig;
