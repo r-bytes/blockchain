2a824dfc826949545b5ecfd96210220e7f4c1cc3
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-09-08 22:51:02 +0200

    feat: lottery entrance

---
+++ nextjs-smartcontract-lottery/src/app/components/LotteryEntrance.tsx
@@ -0,0 +1,14 @@
+import { useWeb3Contract } from "react-moralis";
+
+const LotteryEntrance = () => {
+    // const { runContractFunction: enterRaffle } = useWeb3Contract({
+    //     abi: "",
+    //     contractAddress: "",
+    //     functionName: "",
+    //     params: "",
+    //     msgValue: "",
+
+    // });
+    return <div>LotteryEntrance</div>;
+};
+export default LotteryEntrance;
--- nextjs-smartcontract-lottery/src/app/components/index.js
+++ nextjs-smartcontract-lottery/src/app/components/index.js
@@ -1,2 +1,3 @@
 export { default as Header } from "./Header";
 export { default as ManualHeader } from "./ManualHeader";
+export { default as LotteryEntrance } from "./LotteryEntrance";
