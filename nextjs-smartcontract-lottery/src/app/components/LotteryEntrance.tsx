f76daa10b1a54f860ed46c108b48dfa25f47adc0
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-09-13 12:06:09 +0200

    feat: get entranceFee from contract

--- nextjs-smartcontract-lottery/src/app/components/LotteryEntrance.tsx
+++ nextjs-smartcontract-lottery/src/app/components/LotteryEntrance.tsx
@@ -1,14 +1,50 @@
-import { useWeb3Contract } from "react-moralis";
+import { useMoralis, useWeb3Contract } from "react-moralis";
+import { abi, contractAddresses } from "../../../constants/index";
+import { useEffect, useState } from "react";
+import { ethers, BigNumber } from "ethers";
+
+interface contractAddressesInterface {
+    [key: string]: string[];
+}
 
 const LotteryEntrance = () => {
-    // const { runContractFunction: enterRaffle } = useWeb3Contract({
-    //     abi: "",
-    //     contractAddress: "",
-    //     functionName: "",
-    //     params: "",
-    //     msgValue: "",
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
+    const [entranceFee, setEntranceFee] = useState<string>("");
 
-    // });
-    return <div>LotteryEntrance</div>;
+    const addresses: contractAddressesInterface = contractAddresses;
+    const chainId: number = parseInt(chainIdHex!);
+    const raffleAddress: string | undefined = chainId in addresses ? addresses[chainId][0] : undefined;
+    // console.log("=====> [debug] raffle address: " + raffleAddress)
+
+    const { runContractFunction: enterRaffle } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress!,
+        functionName: "enterRaffle",
+        params: {},
+        msgValue: "",
+    });
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress!,
+        functionName: "getEntranceFee"!,
+        params: {},
+    });
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            (async () => {
+                const entranceFeeFromCall = ((await getEntranceFee()) as BigNumber).toString();
+                setEntranceFee(ethers.utils.formatUnits(entranceFeeFromCall));
+            })();
+        }
+    }, [isWeb3Enabled]);
+    
+    return (
+        <div className="text-xs">
+            {/* LotteryEntrance: <span className="text-xs text-neutral-400">{entranceFee} ETH</span> */}
+            Entrance Fee: <span className="text-xs text-neutral-400">{entranceFee} ETH</span>
+        </div>
+    );
 };
 export default LotteryEntrance;
