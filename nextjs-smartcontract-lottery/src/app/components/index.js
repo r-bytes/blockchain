f3f525f41e9148bf78b132efdedb6194895f6b62
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-09-13 21:58:01 +0200

    feat: spinner component

--- nextjs-smartcontract-lottery/src/app/components/index.js
+++ nextjs-smartcontract-lottery/src/app/components/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header";
 export { default as ManualHeader } from "./ManualHeader";
 export { default as LotteryEntrance } from "./LotteryEntrance";
+export { default as Spinner } from "./Spinner";
