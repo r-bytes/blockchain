a90f65466bdb4edb48e6f5c01fa20ce8cdb96138
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-12-22 22:52:39 +0100

    build: project initialization

--- hardhat-defi-aave/hardhat.config.ts
+++ hardhat-defi-aave/hardhat.config.ts
@@ -1,56 +1,38 @@
-import "@nomicfoundation/hardhat-toolbox"
-import "hardhat-deploy"
-import { HardhatUserConfig } from "hardhat/config"
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomicfoundation/hardhat-verify")
+require("dotenv/config")
 
-import "dotenv/config"
+require("dotenv/config")
 
-const RPC_URL_SEPOLIA: string = process.env.RPC_URL_SEPOLIA || ""
-const RPC_URL_MAINNET: string = process.env.RPC_URL_MAINNET!
-const PRIVATE_KEY: string = process.env.PRIVATE_KEY || ""
-const RPC_URL_GANACHE: string = process.env.RPC_URL_GANACHE || ""
-const PRIVATE_KEY_GANACHE: string = process.env.PRIVATE_KEY_GANACHE || ""
+const RPC_URL_SEPOLIA = process.env.RPC_URL_SEPOLIA || ""
+// const RPC_URL_MAINNET = process.env.RPC_URL_MAINNET;
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY!
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
-const config: HardhatUserConfig = {
-    solidity: {
-        compilers: [
-            {
-                version: "0.8.19",
-            },
-            {
-                version: "0.6.12",
-            },
-            {
-                version: "0.4.19",
-            },
-        ],
-    },
-    mocha: {
-        timeout: 200000,
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: "0.8.20",
+    settings: {
+        optimizer: {
+            enabled: true,
+            runs: 200,
+        },
     },
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
             chainId: 31337,
-            // gasPrice: 130000000000,
-            forking: {
-                url: RPC_URL_MAINNET,
-            },
+            gasPrice: 130000000000,
+            //     forking: {
+            //         url: RPC_URL_MAINNET,
+            // },
         },
         sepolia: {
             chainId: 11155111,
             url: RPC_URL_SEPOLIA,
             accounts: [PRIVATE_KEY],
-        },
-        ganache: {
-            chainId: 1337,
-            url: RPC_URL_GANACHE,
-            accounts: [PRIVATE_KEY_GANACHE],
-        },
-        localhost: {
-            chainId: 31337,
-            url: "http://127.0.0.1:8545/",
         },
     },
     etherscan: {
@@ -59,21 +41,18 @@ const config: HardhatUserConfig = {
         },
         customChains: [],
     },
-    gasReporter: {
-        enabled: false,
-        outputFile: "report-gas.txt",
-        noColors: true,
-        currency: "USD",
-        coinmarketcap: COINMARKETCAP_API_KEY,
-        token: "ETH",
-        // token: "MATIC",
-    },
-    namedAccounts: {
-        deployer: {
-            default: 0, // this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
-        },
+    // gasReporter: {
+    //     enabled: false,
+    //     outputFile: "report-gas.txt",
+    //     noColors: true,
+    //     currency: "USD",
+    //     coinmarketcap: COINMARKETCAP_API_KEY,
+    //     token: "ETH",
+    // },
+    paths: {
+        artifacts: "./artifacts",
+        cache: "./cache",
+        sources: "./contracts",
+        tests: "./test",
     },
 }
-
-export default config
