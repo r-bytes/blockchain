8f1238e64146269ed190472b1e9ab6c2208d0b2d
r-bytes <101632683+r-bytes@users.noreply.github.com>
2024-01-19 10:16:30 +0100

    feat: contract

--- hardhart-nft/contracts/BasicNFT.sol
+++ hardhart-nft/contracts/BasicNFT.sol
@@ -4,5 +4,20 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 pragma solidity ^0.8.7;
 
 contract BasicNFT is ERC721 {
-  constructor() ERC721("Cutie", "CUTIE") {}
+  uint256 private s_tokenCounter;
+  constructor() ERC721("Cutie", "CUTIE") {
+    s_tokenCounter = 0;
+  }
+
+  function mintNft() public returns (uint256) {
+    _safeMint(msg.sender, s_tokenCounter);
+    s_tokenCounter = s_tokenCounter + 1;
+    
+    return s_tokenCounter;
+  }
+
+  function getTokenCounter() public view returns (uint256) {
+    return s_tokenCounter;
+  }
 }
+ 
